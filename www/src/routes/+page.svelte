084c23bf0af5759cc1e1a2dbd59894b011812f88
nawawishkid <nawawish.samerpark@gmail.com>
2023-06-27 15:38:54 +0700

    Update Playwright test case; Add GitHub workflow status badge

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <main class="hero min-h-screen">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
-			<h1 class="font-bold text-5xl mb-8">Welcome!</h1>
+			<h1 class="font-bold text-5xl mb-8">JustChat</h1>
 			<a href="/chat" class="btn btn-primary">Go to chat page</a>
 		</div>
 	</div>
