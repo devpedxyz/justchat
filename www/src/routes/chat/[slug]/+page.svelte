51a4cd3c91aea68711b4060e5c8ba681da513f62
nawawishkid <nawawish.samerpark@gmail.com>
2023-06-29 23:23:28 +0700

    Fix ugly error alert

--- www/src/routes/chat/[slug]/+page.svelte
+++ www/src/routes/chat/[slug]/+page.svelte
@@ -122,20 +122,22 @@
 		<span class="loading loading-ring loading-lg" />
 	</div>
 {:else if errorLoadingConversation}
-	<div class="alert alert-error">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="stroke-current shrink-0 h-6 w-6"
-			fill="none"
-			viewBox="0 0 24 24"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-			/></svg
-		>
-		<span>{errorLoadingConversation.message}</span>
+	<div class="flex items-start justify-around flex-grow p-4">
+		<div class="alert alert-error max-w-[50%]">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>{errorLoadingConversation.message}</span>
+		</div>
 	</div>
 {:else if conversation}
 	<div class="flex flex-col w-full h-full">
