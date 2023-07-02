98261e85003a38dcc688eaf5576766ca512e706d
nawawishkid <nawawish.samerpark@gmail.com>
2023-07-02 19:58:17 +0700

    Bring back the chat sidebar toggler

--- www/src/routes/chat/store.ts
+++ www/src/routes/chat/store.ts
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 
-export const isSidebarOpen = writable(true);
+export const isChatSidebarOpen = writable(true);
 export const header = writable<{ heading?: string }>({ heading: 'JustChat' });
