dc40313891a5a4600d86b16aeaf6eb220b2e067c
nawawishkid <nawawish.samerpark@gmail.com>
2023-07-04 00:46:13 +0700

    Fix incorrect theme flash when reload the page

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
+	import { themeChange } from 'theme-change';
+
+	onMount(() => {
+		themeChange(false);
+	});
 </script>
 
 <slot />
