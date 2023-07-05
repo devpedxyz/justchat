2fd42f729265d25535bb7f0c87c60dd211fbe099
nawawishkid <nawawish.samerpark@gmail.com>
2023-07-05 10:22:53 +0700

    Fix date display in the message box

--- www/src/routes/chat/[slug]/messages-box.svelte
+++ www/src/routes/chat/[slug]/messages-box.svelte
@@ -23,6 +23,32 @@
 			author
 		};
 	});
+	$: messagesGroupedByDate = messagesWithAuthor.reduce((acc, message) => {
+		const date = message.date;
+
+		if (!date) {
+			return acc;
+		}
+
+		let existingMesssages: MessageWithAuthor[];
+		const dayString = date.toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		});
+
+		if (acc.has(dayString)) {
+			existingMesssages = acc.get(dayString)!;
+		} else {
+			existingMesssages = [];
+		}
+
+		existingMesssages.push(message);
+		acc.set(dayString, existingMesssages);
+		return acc;
+	}, new Map() as Map<string, MessageWithAuthor[]>);
+
+	$: console.log('messagesGroupedByDate', messagesGroupedByDate);
 
 	const scroll = (element: HTMLElement) =>
 		element.scroll({ top: element.scrollHeight, behavior: 'smooth' });
@@ -43,24 +69,24 @@
 </script>
 
 <ul bind:this={chatBox} class="flex flex-col gap-2 flex-grow px-4 pt-4 overflow-y-auto min-h-0">
-	{#each messagesWithAuthor as message, idx}
-		{#if messagesWithAuthor[idx - 1] && message.date && messagesWithAuthor[idx - 1].date && message.date.getDay() !== messagesWithAuthor[idx - 1].date?.getDay()}
-			<li class="text-center text-layer/30">{message.date.toDateString()}</li>
-		{/if}
-		<li class="flex gap-2 items-end {message.author_id === user.id ? ' flex-row-reverse' : ''}">
-			<span class="contents">
-				{#if message.author_id !== user.id}
-					<span class="rounded-full w-12 h-12 overflow-hidden">
-						<img src={message.author.user.profile_image_url} alt="User profile" />
-					</span>
+	{#each messagesGroupedByDate as [dateString, messageList]}
+		<li class="text-center text-layer/30 my-8">{dateString}</li>
+		{#each messageList as message}
+			<li class="flex gap-2 items-end {message.author_id === user.id ? ' flex-row-reverse' : ''}">
+				<span class="contents">
+					{#if message.author_id !== user.id}
+						<span class="rounded-full w-12 h-12 overflow-hidden">
+							<img src={message.author.user.profile_image_url} alt="User profile" />
+						</span>
+					{/if}
+					<span class="p-2 rounded bg-chat-msg-bg">{message.message}</span>
+				</span>
+				{#if typeof message.created_at === 'string' && message.created_at.length > 0}
+					<span class="text-layer/30" title={message.date?.toLocaleString()}
+						>{message.created_at}</span
+					>
 				{/if}
-				<span class="p-2 rounded bg-chat-msg-bg">{message.message}</span>
-			</span>
-			{#if typeof message.created_at === 'string' && message.created_at.length > 0}
-				<span class="text-layer/30" title={message.date?.toLocaleString()}
-					>{message.created_at}</span
-				>
-			{/if}
-		</li>
+			</li>
+		{/each}
 	{/each}
 </ul>
